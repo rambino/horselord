2af50aa34d2662d9c94f2646ce9e2f7d2ecdcc5e
rambino <ramy.serageldin@gmail.com>
2016-03-07 17:20:58 -0500

    update img_url

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.get('/net', function(req, res) {
     "text": msg,
     "attachments": [
       {
-          "img_url":"https://giphy.com/gifs/horselord-l2JJClemzArq8PFwQ"
+          "img_url":"https://media.giphy.com/media/l2JJClemzArq8PFwQ/giphy.gif"
       }
     ]
   };
